6ff973dee757fd0006565c325c94dbacb6631274
ZZUN <72775470+soup061222@users.noreply.github.com>
2022-06-15 23:41:10 +0900

    Update buddy.py

--- BUDDY/buddy.py
+++ BUDDY/buddy.py
@@ -2,14 +2,10 @@ import requests
 
 r = requests.get('https://valorant-api.com/v1/buddies?language=ko-KR')
 kor = r.json()
-r = requests.get('https://valorant-api.com/v1/buddies')
-eng = r.json()
 b = kor['data']
-d = eng['data']
 all = len(b)
 counts = 0
 for counts in range(0, all):
     korname = b[counts]['displayName'].upper()
-    engname = d[counts]['displayName'].upper()
     img = b[counts]['displayIcon']
-    print(f'{korname}({engname})\n{img}\n{counts + 1}/{all}')
+    print(f'{korname}\n{img}\n{counts + 1}/{all}')
